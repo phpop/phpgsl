4027e2e6eeb80f45e011af7f34a3484928259067
phpop <42914826+phpop@users.noreply.github.com>
2023-07-12 18:18:02 +0300

    add gsl_complex_arg function in gsl_complex.c

--- gsl_complex.c
+++ gsl_complex.c
@@ -29,14 +29,7 @@ PHP_FUNCTION(gsl_complex_rect)
 
 	z = gsl_complex_rect(x, y);
 	
-	array_init_size(return_value, 2);
-	/*
-	zend_hash_real_init_packed(Z_ARRVAL_P(return_value));
-	ZEND_HASH_FILL_PACKED(Z_ARRVAL_P(return_value)) {
-		ZEND_HASH_FILL_ADD(GSL_REAL(z));
-		ZEND_HASH_FILL_ADD(GSL_IMAG(z));
-	} ZEND_HASH_FILL_END();
-	*/
+	array_init_size(return_value, 2); // массив из 2-х элементов (действит. и мнимая часть)
 	zend_hash_next_index_insert_new(Z_ARRVAL_P(return_value), GSL_REAL(z));
 	zend_hash_next_index_insert_new(Z_ARRVAL_P(return_value), GSL_IMAG(z));
 }
@@ -56,26 +49,52 @@ PHP_FUNCTION(gsl_complex_polar)
 
 	z = gsl_complex_polar(r, theta);
 	
-	array_init_size(return_value, 2);
-	/*
-	zend_hash_real_init_packed(Z_ARRVAL_P(return_value));
-	ZEND_HASH_FILL_PACKED(Z_ARRVAL_P(return_value)) {
-		ZEND_HASH_FILL_ADD(GSL_REAL(z));
-		ZEND_HASH_FILL_ADD(GSL_IMAG(z));
-	} ZEND_HASH_FILL_END();
-	*/
+	array_init_size(return_value, 2); // массив из 2-х элементов (действит. и мнимая часть)
 	zend_hash_next_index_insert_new(Z_ARRVAL_P(return_value), GSL_REAL(z));
 	zend_hash_next_index_insert_new(Z_ARRVAL_P(return_value), GSL_IMAG(z));
 }
 /* }}}*/
 
+/* {{{ double gsl_complex_arg(gsl_complex z)
+ */
+PHP_FUNCTION(gsl_complex_arg)
+{
+	zval *z;
+	gsl_complex val;
+	double x, y;
+	double result;
+
+	ZEND_PARSE_PARAMETERS_START(1, 1)
+		Z_PARAM_ZVAL(z)
+	ZEND_PARSE_PARAMETERS_END();
+	
+	if (Z_TYPE_P(z) != IS_ARRAY) {
+		php_error_docref(NULL, E_WARNING, "Invalid complex number");
+		RETURN_FALSE;
+	}
+
+	x = ZVAL_DOUBLE(zend_hash_index_find(z, 0)); // нулевой элемент - действительная часть
+	y = ZVAL_DOUBLE(zend_hash_index_find(z, 1)); // первый элемент - мнимая часть
+	GSL_SET_COMPLEX(&val, x, y) // устанавливаем значение в комлексное число
+	result = gsl_complex_arg(val);
+	
+	RETURN_DOUBLE(result);
+}
+/* }}} */
+
 /* {{{ arginfo
  */
 ZEND_BEGIN_ARG_INFO(arginfo_gsl_complex_rect, 0)
+	ZEND_ARG_INFO(0, x)
+	ZEND_ARG_INFO(0, y)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_INFO(arginfo_gsl_complex_polar, 0)
-	ZEND_ARG_INFO(0, str)
+	ZEND_ARG_INFO(0, r)
+	ZEND_ARG_INFO(0, theta)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(arginfo_gsl_complex_arg, 0)
+	ZEND_ARG_INFO(0, z)
 ZEND_END_ARG_INFO()
 /* }}} */
 
@@ -84,6 +103,7 @@ ZEND_END_ARG_INFO()
 static const zend_function_entry gsl_complex_functions[] = {
 	PHP_FE(gsl_complex_rect,		arginfo_gsl_complex_rect)
 	PHP_FE(gsl_complex_polar,		arginfo_gsl_complex_polar)
+	PHP_FE(gsl_complex_arg,		arginfo_gsl_complex_arg)
 	PHP_FE_END
 };
 /* }}} */
